4df84f037b17805cb8ce98ab9e25e349e4e986cf
ayomide <ayomidekareem563@gmail.com>
2024-08-05 12:18:58 -0700

    order form working

--- src/orderpage/orderform.jsx
+++ src/orderpage/orderform.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../orderpage/orderform.css"
 import formicon from "../img/formicon.png"
 import Item from "./item";
 export default function Orderform(){
+    const[items,setitems]=useState([])
+    
+    useEffect(()=>{
+        var storedItems=JSON.parse(localStorage.getItem("items"))
+        setitems(storedItems)
+    },[items])
+
     return <div className="formMain">
         <div className="formBorder">
         <section className="formTitle">
             <p>Order From</p>
             <h3>Mama J Bukka</h3>
         </section>
-        <section className="formBody">
-            <img src={formicon} alt="" />
-            <h4>No Item Yet</h4>
-            <h5>Looks Like You Havent Made Your Order Yet</h5>
+
+        {items.length>0?
+        <section>
+        {items.map((item)=>(
+        <div key={item.name}>
+        <section className="">
+        <img src={item.image} alt="" />
+        <div>
+            <h3>{item.name}</h3>
+            <h4>${item.price}.00</h4>
+        </div>
         </section>
+        </div>
+        ))}
+    </section>:
+        <section className="formBody">
+        <img src={formicon} alt="" />
+        <h4>No Item Yet</h4>
+        <h5>Looks Like You Havent Made Your Order Yet</h5>
+    </section>
+        }
+
         <section className="btnsection">
         <button>SUBMIT ORDER</button>
         </section>
